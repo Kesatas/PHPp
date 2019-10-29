4018a5146370baf88b9e259f2e579e2157167160
kestas <kesatas@yahoo.com>
2019-10-29 16:45:19 +0200

    .
    .

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <html>
     <head>
-        <title> <?php print 'Aš '.date('l').' ir PHP' ?></title>
+        <title> <?php print 'Aš '.date('l').' ir PHP';?></title>
     </head>
     <body>
-        <h1><b>Kestas</B>  - HTML <?php print 'ir PHP'; ?> asas</h1>
+        <h1><b>Kestas</B>  - HTML <?php print 'ir PHP';?> asas</h1>
         <p>jau nuo <?php print date('Y').' metų';?></p>
         <p>
             Viskas prasidėjo <?php print date('F').' mėnesio,'. date('d').' dieną';?>
