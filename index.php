27e0ebb1d5ae33db73ece6554fceccadb83f8346
kestas <kesatas@yahoo.com>
2019-10-29 15:44:26 +0200

    V2
    .

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
+<?PHP
+?>
 <html>
     <head>
-        <title>As valdau HTML</title>
+        <title>As valdau HTML ir PHP</title>
     </head>
     <body>
-        <h1>Kestas </h1> - HTML asas
-        <p>moku kurti statini turini </p>
+        <h1><b>Kestas</B>  - HTML ir PHP asas </h1>
+        <p>Kuriu dinaminį turinį. </p>
+        <p>
+            Atidaryta :<?PHP print date('2019-10-29 15:43'); ?>
+        </p>
     </body>
 </html>
