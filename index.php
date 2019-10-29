dc6d0cf94791d098ae4086bab9581717ad805fee
kestas <kesatas@yahoo.com>
2019-10-29 15:07:26 +0200

    html
    HTML

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
-<?php
-print 'veikia puikiai !';
+<html>
+    <head>
+        <title>As valdau HTML</title>
+    </head>
+    <body>
+        <h1>Kestas </h1> - HTML asas
+        <p>moku kurti statini turini </p>
+    </body>
+</html>
