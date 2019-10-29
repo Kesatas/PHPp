ceb39be2ab400142d7bac401afe32249da5c3343
kestas <kesatas@yahoo.com>
2019-10-29 17:32:24 +0200

    .
    .

--- index.php
+++ index.php
@@ -5,11 +5,7 @@
     <body>
         <h1>Dainu tekstai</h1>
         <h2>Paragrafo daina</h2>
-        <p>
-           aš toks vienišas jaunas paragrafas....
-        </p>
-        <p>
-           Mindau svetimo miesto gatves....
-        </p>
+        <p>aš toks vienišas jaunas paragrafas....</p>
+        <!--<p>Mindau svetimo miesto gatves....</p> -->
     </body>
 </html>
