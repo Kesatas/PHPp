fe3156a5c8ec309b33aecdf14cf3f152da38ab88
kestas <kesatas@yahoo.com>
2019-10-30 16:09:05 +0200

    dice
    kauliukas

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
+<?php
+?>
 <html>
     <head>
         <title>
             PHP lydės ir <?php print date('Y', strtotime('+'.rand(1,10).'Years'));?>
-        </title>
+        </title> 
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        <h1>Galbut turesiu <?php print rand(1,5).' vaikus'?></h1>
-        <p>D.Trump'as nebebus prezidentu :
-        <?php 
-        print date('Y', strtotime('+'. rand(2,10).'Years')).' '. date('l').' '. date('d');
-        ?>
-        </p>
+        <div class="class<?php print rand(1,6);?>">
+        </div>
     </body>
+
 </html>
