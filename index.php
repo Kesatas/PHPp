e8d18aaaf624758bc8f868320dde5e826a18f542
kestas <kesatas@yahoo.com>
2019-10-30 17:21:15 +0200

    bomba su sprogimu
    bomba su sprogimu css

--- index.php
+++ index.php
@@ -9,8 +9,25 @@ $sekundes = date('s');
     </head>
     <body>
         <main>
-            <img src="/images/bomb.png" alt="" height="<?php print $aukstis;?>" width="<?php print $plotis;?>">
+            <div class="bomba"></div>
+            <div class="bomba-<?php print $sekundes;?>"></div>
             <h1><?php print $sekundes; ?></h1>
         </main>
+        <style>
+            .bomba{
+                 background-image: url(/images/bomb.png);
+                 background-position: center;
+                 height : <?php print $aukstis;?>;
+                 width :<?php print $plotis;?>;
+                 background-size :cover;
+            }
+            .bomba-00{
+                 background-image: url(/images/boom.jpg);
+                 background-position: center;
+                 height : 500px ;
+                 width : 500px;
+                 background-size :cover;
+            }
+        </style>
     </body>
 </html>
