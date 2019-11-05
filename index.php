b0a629190495d8b7ec10b780a43d2d07eb4e799f
kestas <kesatas@yahoo.com>
2019-11-05 17:22:40 +0200

    laikas
    as

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 $days = 365;
 $pack_price = 3.50;
 $count_ttl = 0;
+$time_per_cig = 5;
+
 
 for ($i = 0; $i < $days; $i++) {
     $date = date('N', strtotime("+$i day"));
@@ -15,13 +17,17 @@ for ($i = 0; $i < $days; $i++) {
 }
 if ($count_ttl % 20) {
     $price_ttl = round($count_ttl / 20) * $pack_price;
-}
-else{
+} else {
     $price_ttl = round($count_ttl / 20) * $pack_price;
     $price_ttl += $pack_price;
 }
+
+$time_total = round(($count_ttl / 5) / 60);
+
+
 $h1 = 'Mano dumu skaiciuokle';
 $h2 = "Per $days dienas , surukysiu $count_ttl cigareciu uz $price_ttl eur.";
+$h3 = "Viso traukdamas prastovesiu $time_total valandu "
 ?>
 <html>
     <head>
@@ -30,5 +36,6 @@ $h2 = "Per $days dienas , surukysiu $count_ttl cigareciu uz $price_ttl eur.";
     <body>
         <h1><?php print $h1; ?></h1>
         <h2><?php print $h2; ?></h2>
+        <h3><?php print $h3; ?></h3>
     </body>
 </html>
