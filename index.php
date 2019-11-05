fa0fb54effe9b58964c7525656b76e650c898178
kestas <kesatas@yahoo.com>
2019-11-05 15:47:18 +0200

    month expenses.
    //

--- index.php
+++ index.php
@@ -1,28 +1,23 @@
 <?php
-$kates = rand(1, 3);
-$sunys = rand(1, 3);
-$katasuniai = 0;
+$months = 12;
+$wallet = 1000;
+$month_income = 700;
 
-for ($i = 0; $i < $kates; $i++) {
-    for ($j = 0; $j < $sunys; $j++) {
-        $pavyko = rand(0, 1);
-        if ($pavyko) {
-            $katasuniai++;
-            break;
-        }
-    }
+for ($i = 0; $i < $months; $i++) {
+    $month_expenses = rand(500, 700);
+    $wallet += $month_income;
+    $wallet - $month_expenses;
 }
 
-$h1 = 'Katasuniu Iseiga';
-$h2 = "Dalyvavo $kates kates ir $sunys Sunys";
-$h3 = "Katasuniu iseiga : $katasuniai";
+$h1 = 'Wallet Forecast';
+$h2 = "Po $months m.,prognozuojamas likutis $wallet";
 ?>
 <html>
     <head>
+        <title>Cycles</title>
     </head>
     <body>
         <h1><?php print $h1; ?></h1>
         <h2><?php print $h2; ?></h2>
-        <h3><?php print $h3; ?></h3>
     </body>
 </html>
