bf927716e9fe809ef4742dc97b38019526afda63
kestas <kesatas@yahoo.com>
2019-11-05 14:35:05 +0200

    ford
    d

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
 <?php
-$x = rand(10,20);
 
-for ($x; $x>0; $x--)
-{
-    var_dump($x);
+$x = rand(10, 20);
+
+for ($x; $x > 0; $x--) {
+    var_dump ('Tai yra'.$x.'-tasis ciklas');
 }
 ?>
-<html>
-    <head>
-    </head>
-    <body>
-    </body>
-</html>
