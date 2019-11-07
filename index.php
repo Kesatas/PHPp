01e08fe73a0df4ce45f774bd265df12874b68e63
kestas <kesatas@yahoo.com>
2019-11-07 14:52:28 +0200

    as
    sadsda

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
 
 $produktas = [
-    'didelis' => ['kiausinis', 'kiausinis', 'Grietines indelis'],
-    'mazas' => ['baklazanas']
+    $kiausinis = [
+        'mazas' => '',
+        'didelis' => ['kiausinis','kiausinis'],
+    ],
+    $baklazanas = [
+        'mazas' => ['baklazanas'],
+        'didelis' => '',
+    ],
+    $grietine = [
+        'mazas' => '',
+        'didelis' => ['indelis'],
+    ],
 ];
 
 var_dump($produktas);
